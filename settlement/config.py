ee28f445040b92bb9794958c589f6afc90e1f8b3
WorIddy <woohuiso@gmail.com>
2025-11-15 13:38:55 +0900

    created
    seperated_app은 혹시나 app.py가 안되면 하나하나 보시라고 추가합니다>

--- settlement/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_PATH = os.path.join(BASE_DIR, "settlement.db")
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
