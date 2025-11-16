5888d4a1ba61752d575d22cda893a95c90556cae
wnsdn1583 <wnsdn1583@naver.com>
2025-11-16 23:20:33 +0900

    FIX X-USER-ID 유무 확인 코드 수정

--- was/app.py
+++ was/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, g
 import requests 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.before_request
 def before_request():
@@ -9,14 +9,14 @@ def before_request():
     # X-User-ID 유무에 따라 navbar 우측 layout이 달라짐 (layout.html 확인)
     g.user_id = request.headers.get('X-User-ID') 
     if g.user_id:
-        api_url = f"http://account/account/balance?user_id={g.user_id}"
-    try:
-        response = requests.get(api_url, timeout=5)
-        response.raise_for_status() # HTTP 오류가 발생하면 예외 발생
-        data = response.json()
-        g.user_balance = data.get("balance", "N/A") # 'balance' 키의 값을 반환
-    except requests.exceptions.RequestException as e:
-        g.user_balance = " "
+        api_url = f"http://account:5000/account/balance?user_id={g.user_id}"
+        try:
+            response = requests.get(api_url, timeout=5)
+            response.raise_for_status() # HTTP 오류가 발생하면 예외 발생
+            data = response.json()
+            g.user_balance = data.get("balance", "N/A") # 'balance' 키의 값을 반환
+        except requests.exceptions.RequestException as e:
+            g.user_balance = " "
 
 @app.route('/')
 def index():
@@ -37,14 +37,20 @@ def register():
 
 @app.route('/web/deposit')
 def deposit():
+    if not g.user_id:
+        return redirect(url_for('login'))
     return render_template('deposit.html')
 
 @app.route('/web/withdraw')
 def withdraw():
+    if not g.user_id:
+        return redirect(url_for('login'))
     return render_template('withdraw.html')
 
 @app.route('/web/fund')
 def fund():
+    if not g.user_id:
+        return redirect(url_for('login'))
     return render_template('fund.html')
 
 if __name__ == '__main__':
